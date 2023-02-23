326395d607d42822fe200e3110f67083451219fc
cristian martinez <cristianjesusmb@gmail.com>
2023-02-23 15:55:31 -0500

    cabios en el limite de carga en las peticiones

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,38 @@
-import express, {Application} from "express";
-import cors from 'cors'
-
+import express, { Application } from "express"; // importamos express y Application desde express
+import cors from 'cors' // importamos cors
 
 class Server {
 
-    private app:Application;
-    private port:string;
-    private apiPaths ={
-     
+    private app: Application; // declaramos la variable app como tipo Application
+    private port: string; // declaramos la variable port como tipo string
+    private apiPaths = { // definimos el objeto apiPaths que contendrá las rutas
+
     }
 
-    constructor (){
-    //constructor del servidor express
-    this.app = express();
-    //puerto de conexion
-    this.port = process.env.PORT  || '80';
-    //database 
+    constructor() { // constructor de la clase
+        this.app = express(); // instanciamos express y lo asignamos a la variable app
+        this.port = process.env.PORT || '80'; // asignamos el puerto de conexión, tomándolo de la variable de entorno PORT, o usando el puerto 80 por defecto si no está definido
 
-    // Definicion de middlewares
-    this.middlewares();
-
-    this.routes();
+        // Definicion de middlewares
+        this.middlewares(); // llamamos al método middlewares
+        this.routes(); // llamamos al método routes
     }
 
-
-
-    middlewares() {
-    //CORS
-    this.app.use(cors());
-    // Lectra del body
-    this.app.use(express.json({limit: '2mb'}));
-
-    // Carpeta publica
-     this.app.use(express.static('public'));
-    }
- 
-    routes() {
-  
-
-  
+    middlewares() { // método que define los middlewares
+        this.app.use(cors()); // permite peticiones CORS desde cualquier origen
+        this.app.use(express.json({ limit: '5mb' })); // permite leer el cuerpo de peticiones con formato JSON con un límite de 5mb
+        this.app.use(express.static('public')); // sirve archivos estáticos desde la carpeta public
     }
 
-    
-
-    listen() {
-    //la app selecciona el puerto asignado y empieza a correr el servidor
-
-    this.app.listen(this.port,()=>{
-        console.log('Servidor corriendo en el puerto  ' + this.port);
-    })
+    routes() { // método que define las rutas
+        // aquí se definen las rutas usando this.app.use
     }
 
-
-
+    listen() { // método que arranca el servidor
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el puerto ' + this.port); // imprime en consola un mensaje indicando que el servidor se está ejecutando
+        })
+    }
 }
-export default Server;
+
+export default Server; // exporta la clase Server
